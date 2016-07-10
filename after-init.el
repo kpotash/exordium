80b650ba9873603d8ea4183837f4007ef25f25e7
Abhik Nandi <abhik.nandi@hotmail.com>
2016-07-09 23:56:29 -0400

    add customizations

---
+++ after-init.el
@@ -0,0 +1,32 @@
+;; ======================================== set keys
+(global-set-key "\C-ca" 'org-agenda)
+
+(global-set-key [(meta q)] 'indent-region)
+(global-set-key [insert]   'overwrite-mode)
+
+(global-set-key [f1] 'helm-swoop)
+(global-set-key [(S-f1)] 'helm-projectile-ag)
+(global-set-key [f2] 'split-window-vertically)
+(global-set-key [f3] 'kill-region)
+(global-set-key [f4] 'kill-ring-save)
+(global-set-key [f5] 'yank)
+
+(global-set-key [f6] 'bury-buffer)
+(global-set-key [f7] 'other-window)
+(global-set-key [f8] 'yas-expand)
+(global-set-key [f9] 'next-error)
+(global-set-key [(shift f9)] 'previous-error)
+;(global-set-key [f10] 'compile)
+;(global-set-key [(shift f10)] 'make-clean)
+(global-set-key [f11] 'goto-last-change)
+;(global-set-key [(S-f11)] 'magit-status)
+(global-set-key [f12] 'find-grep)
+
+(setq ac-sources '(ac-source-my-rtags ac-source-my-c-headers ac-source-words-in-all-buffer))
+(global-auto-complete-mode t)
+(setq ac-dwim t)
+(setq ac-ignore-case nil)
+(setq ac-delay 0)
+(setq ac-auto-show-menu t)
+(setq ac-disable-faces nil)
+;(setq ac-auto-start 2)
---
+++ prefs.el
@@ -0,0 +1,3 @@
+(set-default-font "-unknown-Inconsolata-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")
+
+(setq exordium-helm-everywhere t)
